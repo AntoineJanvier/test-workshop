4a3bfd4e9910a6b924b6d48298e097b36c4baeaf
Vincent Talloir <vincenttall@hotmail.fr>
2017-06-11 22:09:03 +0200

    test users isEqual

--- src/test/java/com/example/integration/UserIntegrationTest.java
+++ src/test/java/com/example/integration/UserIntegrationTest.java
@@ -53,5 +53,40 @@ public class UserIntegrationTest extends DatabaseTest {
         assertThat(counter).isEqualTo(1);
         assertThat(userRepository.getUser(user)).isEqualTo(null);
     }
-    
+
+
+    @Test
+    public void should_users_is_equal() {
+        final User user1 = User.builder()
+                .name("boby")
+                .role(Role.USER)
+                .password("toto")
+                .build();
+
+        final User user2 = User.builder()
+                .name("jony")
+                .role(Role.USER)
+                .password("toto")
+                .build();
+
+        final User user3 = User.builder()
+                .name("boby")
+                .role(Role.USER)
+                .password("toto")
+                .build();
+
+        int beforeCounter = countOnTable("user");
+
+        userRepository.insertUser(user1);
+        userRepository.insertUser(user2);
+        userRepository.insertUser(user3);
+
+        int counter = countOnTable("user");
+
+
+        assertThat(beforeCounter).isEqualTo(0);
+        assertThat(counter).isEqualTo(3);
+        assertThat(userRepository.getUser(user1).isEqual(userRepository.getUser(user3))).isEqualTo(true);
+        assertThat(userRepository.getUser(user1).isEqual(userRepository.getUser(user2))).isEqualTo(false);
+    }
 }
